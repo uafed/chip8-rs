03120213fade26e6bb45d1a3a2ced1623b0525d8
UAFederez <uafederez@gmail.com>
2026-03-11 22:12:11 +0800

    feat: add command parsing and time-based drawing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![allow(dead_code)]
 
+use clap::{Parser, Subcommand};
+
 use std::{
     io::{BufWriter, Result, Write, stdout},
-    time::Duration,
+    time::{Duration, Instant},
 };
 
 use crossterm::{
@@ -12,13 +14,34 @@ use crossterm::{
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    FromRomFile(RomFileArgs),
+}
+
+#[derive(Parser, Debug)]
+struct RomFileArgs {
+    path: String,
+}
+
 fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let Commands::FromRomFile(RomFileArgs { .. }) = &cli.command;
+
     let mut stdout = BufWriter::new(stdout());
 
     terminal::enable_raw_mode()?;
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(cursor::Hide)?;
 
+    let time = Instant::now();
     loop {
         if event::poll(Duration::ZERO)? {
             match event::read()? {
@@ -29,9 +52,10 @@ fn main() -> Result<()> {
 
         for y in 0..32 {
             stdout.queue(cursor::MoveTo(0, y))?;
-            for x in 0..64 {
-                let r = ((x as f32 / 64.0f32) * 255.0) as u8;
-                let g = ((y as f32 / 64.0f32) * 255.0) as u8;
+            for _ in 0..64 {
+                let t = time.elapsed().as_secs_f32();
+                let r = ((t - t.floor()) * 255.0) as u8;
+                let g = ((((y as f32) / 64.0f32) % 64.0) * 255.0) as u8;
                 let b = (0.2 * 255.0) as u8;
                 stdout.queue(SetBackgroundColor(Color::Rgb { r, g, b }))?;
                 stdout.write_all(b" ")?;
