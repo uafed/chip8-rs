10bec11ae3205ded929ee998049c1e1d9b8b3235
UAFederez <uafederez@gmail.com>
2026-03-11 21:46:53 +0800

    feat: implement basic 2d buffer terminal rendering

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,50 @@
 #![allow(dead_code)]
 
-enum Instruction {
-    ClearDisplay,
-    Return,
-    Jump(u16),
-    Call(u16),
-}
+use std::{
+    io::{BufWriter, Result, Write, stdout},
+    time::Duration,
+};
 
-fn main() {
-    let _memory: [u8; 4096] = [0; 4096];
-    println!("Hello, world!");
+use crossterm::{
+    ExecutableCommand, QueueableCommand, cursor,
+    event::{self, Event, KeyCode},
+    style::{Color, ResetColor, SetBackgroundColor},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+fn main() -> Result<()> {
+    let mut stdout = BufWriter::new(stdout());
+
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(cursor::Hide)?;
+
+    loop {
+        if event::poll(Duration::ZERO)? {
+            match event::read()? {
+                Event::Key(k) if k.code == KeyCode::Char('q') => break,
+                _ => {}
+            }
+        }
+
+        for y in 0..32 {
+            stdout.queue(cursor::MoveTo(0, y))?;
+            for x in 0..64 {
+                let r = ((x as f32 / 64.0f32) * 255.0) as u8;
+                let g = ((y as f32 / 64.0f32) * 255.0) as u8;
+                let b = (0.2 * 255.0) as u8;
+                stdout.queue(SetBackgroundColor(Color::Rgb { r, g, b }))?;
+                stdout.write_all(b" ")?;
+            }
+        }
+
+        stdout.queue(ResetColor)?;
+        stdout.flush()?;
+    }
+
+    terminal::disable_raw_mode()?;
+    stdout.execute(LeaveAlternateScreen)?;
+    stdout.execute(cursor::Show)?;
+
+    Ok(())
 }
