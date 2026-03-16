4eb7d0dc6720933553e11659fbeba724e18e70d0
UAFederez <uafederez@gmail.com>
2026-03-16 10:25:04 +0800

    feat: add logical instruction parsing

--- src/assembler/parse_logical.rs
+++ src/assembler/parse_logical.rs
@@ -1,7 +1,64 @@
-use nom::IResult;
+use nom::{
+    IResult, Parser, branch::alt, bytes::complete::tag_no_case, character::complete::space1,
+    combinator::map, sequence::separated_pair,
+};
 
-use crate::Logical;
+use crate::{
+    Logical,
+    assembler::primitives::{arguments_separator, parse_general_register},
+};
 
 pub fn parse_logical_instruction(input: &str) -> IResult<&str, Logical> {
-    todo!();
+    enum Command {
+        Or,
+        And,
+        Xor,
+        Shr,
+        Shl,
+    }
+
+    let (input, (command, _)) = (
+        alt((
+            map(tag_no_case("or"), |_| Command::Or),
+            map(tag_no_case("and"), |_| Command::And),
+            map(tag_no_case("xor"), |_| Command::Xor),
+            map(tag_no_case("shr"), |_| Command::Shr),
+            map(tag_no_case("shl"), |_| Command::Shl),
+        )),
+        space1,
+    )
+        .parse(input)?;
+
+    let (input, (x_register, y_register)) = separated_pair(
+        parse_general_register,
+        arguments_separator,
+        parse_general_register,
+    )
+    .parse(input)?;
+
+    Ok((
+        input,
+        match command {
+            Command::Or => Logical::OrRegisterXWithRegisterY {
+                x_register,
+                y_register,
+            },
+            Command::And => Logical::AndRegisterXWithRegisterY {
+                x_register,
+                y_register,
+            },
+            Command::Xor => Logical::XorRegisterXWithRegisterY {
+                x_register,
+                y_register,
+            },
+            Command::Shl => Logical::ShiftRegisterXLeftWithRegisterY {
+                x_register,
+                y_register,
+            },
+            Command::Shr => Logical::ShiftRegisterXRightWithRegisterY {
+                x_register,
+                y_register,
+            },
+        },
+    ))
 }
