2e85ec3f027f267a2ed9526ab53f2e6a4d7e5ce0
UAFederez <uafederez@gmail.com>
2026-03-18 23:24:24 +0800

    feat: encode control flow instructions to binary

--- src/assembler/encode_control_flow.rs
+++ src/assembler/encode_control_flow.rs
@@ -1,5 +1,23 @@
 use crate::ControlFlow;
 
 pub fn encode_control_flow(instruction: &ControlFlow) -> u16 {
-    todo!();
+    match instruction {
+        &ControlFlow::SkipNextIfRegisterXEqualsImmediate { x_register, value } => {
+            (0x3000) | ((x_register as u16) << 8) | (value as u16)
+        }
+        &ControlFlow::SkipNextIfRegisterXNotEqualsImmediate { x_register, value } => {
+            (0x3000) | ((x_register as u16) << 8) | (value as u16)
+        }
+        &ControlFlow::SkipNextIfRegisterXEqualsRegisterY {
+            x_register,
+            y_register,
+        } => 0x5000 | ((x_register as u16) << 8) | ((y_register as u16) << 4),
+        &ControlFlow::SkipNextIfRegisterYNotEqualRegisterX {
+            x_register,
+            y_register,
+        } => 0x9000 | ((x_register as u16) << 8) | ((y_register as u16) << 4),
+        &ControlFlow::JumpToAddress { address } => 0x1000 | address,
+        &ControlFlow::CallSubroutine { address } => 0x2000 | address,
+        ControlFlow::ReturnFromSubroutine => 0x00EE,
+    }
 }
